fc933a88a0d475b13ee612128286cd1d4c46fa64
Ahmad Hammad <afhammad@gmail.com>
2011-02-16 17:27:37 +0200

    follow.rb was not being required

--- lib/acts_as_followable.rb
+++ lib/acts_as_followable.rb
@@ -1,7 +1,9 @@
 %w( acts_as_followable/version
-    acts_as_followable/followable
+    acts_as_followable/followable  
 ).each do |lib|
-    require File.join(File.dirname(__FILE__), lib)
+    require File.join(File.dirname(__FILE__), lib)  
 end
+
+require File.join(File.dirname(__FILE__), '../app/models/follow.rb')
 
 ActiveRecord::Base.send(:include, ActsAsFollowable::Followable)
